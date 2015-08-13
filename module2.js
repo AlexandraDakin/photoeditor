4bf317f224f5fdd74d9094bbe7693eab060dccbc
V3507 (Conference ID) <v3507@cam.ac.uk>
2015-08-13 15:48:18 +0100

    photo filters #2 13/08/2015

--- module2.js
+++ module2.js
@@ -26,13 +26,103 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function definitions here
+class RGBA {
+    constructor(redValue, greenValue, blueValue, alphaValue) {
+        this.red = redValue;
+        this.green = greenValue;
+        this.blue = blueValue;
+        this.alpha = alphaValue;
+    }
+}
+
 
 $(document).ready(function() {
     var img = new Image();
-    img.src = "img/cat.jpg";
+    img.src = "img/Jackson2.JPG";
 
+    var color = new RGBA(225, 100, 50, 255);
+    console.log(color.red);
+    console.log(color.green);
+    console.log(color.blue);
+    console.log(color.alpha);
 
-
-
+    colourise(img, color, 40);
+    sepia(img);
 });
+
+
+class Point {
+    constructor(xValue, yValue) {
+        this.x = xValue;
+        this.y = yValue;
+    }
+}
+
+var point = new Point(1, 2);
+console.log(point);
+
+
+
+function colourise(img, colour, level) {
+
+    var pixels = ImageUtils.getPixels(img);
+    var all = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < all; i += 4) {
+
+        var originalRGBA = new RGBA(data[i], data[i+2], data[i+3]);
+        var modifiedRGBA = colourisePixel(originalRGBA, colour, level);
+
+        data[i] = modifiedRGBA.red;
+        data[i+1] = modifiedRGBA.green;
+        data[i+2] = modifiedRGBA.blue;
+        data[i+3] = modifiedRGBA.alpha;
+    }
+    ImageUtils.putPixels(pixels, img.width, img.height)
+}
+
+function colourisePixel(originalRGBA, colour, level) {
+
+    var diffRed = (originalRGBA.red - colour.red) * (level / 100);
+    var modifiedRed = originalRGBA.red - diffRed;
+
+    var diffGreen = (originalRGBA.green - colour.green) * (level / 100);
+    var modifiedGreen = originalRGBA.green - diffGreen;
+
+    var diffBlue = (originalRGBA.blue - colour.blue) * (level / 100);
+    var modifiedBlue = originalRGBA.blue - diffBlue;
+
+    return new RGBA(modifiedRed, modifiedGreen, modifiedBlue, colour.alpha);
+}
+
+function sepiaPixel(colour) {
+
+    var modifiedRed = colour.red * 0.393 + colour.green * 0.769 + colour.blue * 0.189;
+    var modifiedGreen = colour.red * 0.349 + colour.green * 0.686 + colour.blue * 0.168;
+    var modifiedBlue = colour.red * 0.272 + colour.green * 0.534 + colour.blue * 0.131;
+
+    return new RGBA(modifiedRed, modifiedGreen, modifiedBlue, colour.alpha);
+}
+
+function sepia(img) {
+
+    var pixels = ImageUtils.getPixels(img);
+    var all = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < all;i +=4) {
+        var originalRGBA = new RGBA(data[i], data[i+1], data[i+2], data[i+3]);
+        var sepiaRGBA = sepiaPixel(originalRGBA);
+
+        data[i] = sepiaRGBA.red;
+        data[i+1] = sepiaRGBA.green;
+        data[i+2] = sepiaRGBA.blue;
+        data[i+3] = sepiaRGBA.alpha;
+    }
+
+    ImageUtils.putPixels(pixels, img.width, img.height);
+
+}
+// function definitions here
+
